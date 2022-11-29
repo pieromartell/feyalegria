a105a45b16b70083c0bbea525518f10f0b165c5a
Piero Martell Falcon <105953521+Pierom689@users.noreply.github.com>
2022-11-28 20:15:43 -0500

    Creacion del proyecto
    Es la Creacion del proyecto

---
+++ app/src/main/java/com/example/feyalegria/CargaActivity.java
@@ -0,0 +1,20 @@
+package com.example.feyalegria;
+
+//Este es la pantalla de carga
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class CargaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //Aqui se infica que primero carge la Mainactivity
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
